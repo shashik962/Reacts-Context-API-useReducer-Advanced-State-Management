b0538d8499503a2e2cd5005e67ea222b2cf0916a
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-28 22:18:42 +0530

    Creating & Providing The Context

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import Product from './components/Product.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
-
+import { CartContext } from './store/shopping-cart-context.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -68,7 +68,7 @@ function App() {
   }
 
   return (
-    <>
+    <CartContext.Provider>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -80,7 +80,7 @@ function App() {
             </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
